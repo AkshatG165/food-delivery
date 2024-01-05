e062c4b987e533d7217be78750207823e278a526
AkshatG165 <guptaakshat3011@gmail.com>
2024-01-06 00:07:09 +0530

    fix: Styling of root layout

--- components/layout/RootLayout.tsx
+++ components/layout/RootLayout.tsx
@@ -2,9 +2,9 @@ import React, { ReactNode } from 'react';
 import classes from './RootLayout.module.css';
 import MainNavigation from './MainNavigation';
 
-interface Props {
+type Props = {
   children: React.ReactNode;
-}
+};
 
 const RootLayout: React.FC<Props> = (props) => {
   return (

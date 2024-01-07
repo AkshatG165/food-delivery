74469073a1062009c52d1138ccaa3c507042ba17
AkshatG165 <guptaakshat3011@gmail.com>
2024-01-07 16:22:41 +0530

    style: add cart icon

--- components/layout/MainNavigation.tsx
+++ components/layout/MainNavigation.tsx
@@ -1,6 +1,7 @@
 import classes from './MainNavigation.module.css';
 import Link from 'next/link';
 import logo from '../../public/logo.png';
+import cartIcon from '../../public/cart-icon.png';
 
 export default function MainNavigation() {
   return (
@@ -14,7 +15,10 @@ export default function MainNavigation() {
             <Link href="/orders">Orders</Link>
           </li>
           <li>
-            <Link href="/cart">Cart (3)</Link>
+            <Link href="/cart">
+              <img src={cartIcon.src} alt="cart-icon" />
+              Cart
+            </Link>
           </li>
         </ul>
       </nav>

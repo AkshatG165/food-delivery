f1382da5ee72cf69dab6192801240fc31de8a277
AkshatG165 <guptaakshat3011@gmail.com>
2024-01-10 03:33:11 +0530

    feat: model class for orders

---
+++ model/Order.ts
@@ -0,0 +1,48 @@
+import Item from './Item';
+import User from './User';
+
+export default class Order {
+  user: User;
+  items: Item[];
+  shippingAddress: {
+    fullName: string;
+    address: string;
+    city: string;
+    postalCode: string;
+    country: string;
+  };
+  totalPrice: number;
+  paymentMethod: string;
+  paymentResult: { id: string; status: string; email: string };
+  isDelivered: boolean;
+  orderDateTime: Date;
+  deliveredAt: Date;
+
+  constructor(
+    user: User,
+    items: Item[],
+    shippingAddress: {
+      fullName: string;
+      address: string;
+      city: string;
+      postalCode: string;
+      country: string;
+    },
+    totalPrice: number,
+    paymentMethod: string,
+    paymentResult: { id: string; status: string; email: string },
+    isDelivered: boolean,
+    orderDateTime: Date,
+    deliveredAt: Date
+  ) {
+    this.user = user;
+    this.items = items;
+    this.shippingAddress = shippingAddress;
+    this.totalPrice = totalPrice;
+    this.paymentMethod = paymentMethod;
+    this.paymentResult = paymentResult;
+    this.isDelivered = isDelivered;
+    this.orderDateTime = orderDateTime;
+    this.deliveredAt = deliveredAt;
+  }
+}

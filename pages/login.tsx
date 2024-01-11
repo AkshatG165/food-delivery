3a3894883871c98d17b51f4036b89a80b03694dd
AkshatG165 <guptaakshat3011@gmail.com>
2024-01-12 02:03:28 +0530

    feat: add route protection for login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,5 +1,27 @@
 import LoginForm from '@/components/login/LoginForm';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { getServerSession } from 'next-auth';
+import { authOptions } from './api/auth/[...nextauth]';
 
 export default function Login() {
   return <LoginForm />;
+}
+
+type Context = {
+  req: NextApiRequest;
+  res: NextApiResponse;
+};
+
+export async function getServerSideProps({ req, res }: Context) {
+  const session = await getServerSession(req, res, authOptions);
+
+  if (session) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
+  return { props: {} };
 }

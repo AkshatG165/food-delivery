875de238b4962a660ae3aff94f42d76a5d795110
AkshatG165 <guptaakshat3011@gmail.com>
2024-01-14 19:35:49 +0530

    style: Fix space between text

--- components/cart/PriceDetails.tsx
+++ components/cart/PriceDetails.tsx
@@ -11,19 +11,21 @@ export default function PriceDetails({ cartTotal }: { cartTotal: number }) {
     <Card className={classes['price-details']}>
       <h2>Price Details</h2>
       <div>
-        <span>Subtotal</span>
+        <span className={classes.text}>Subtotal</span>
         <span>{cartTotal.toFixed(2)}</span>
       </div>
       <div>
-        <span>GST</span>
+        <span className={classes.text}>GST</span>
         <span>{gst.toFixed(2)}</span>
       </div>
       <div>
-        <span>Delivery Charge</span>
-        <span>{deliveryCharge === 0 ? 'Free!' : deliveryCharge}</span>
+        <span className={classes.text}>Delivery Charge</span>
+        <span className={classes['delivery-charge']}>
+          {deliveryCharge === 0 ? 'Free' : deliveryCharge}
+        </span>
       </div>
       <div className={classes['grand-total']}>
-        <span>Grand Total</span>
+        <span className={classes.text}>Grand Total</span>
         <span>{grandTotal.toFixed(2)}</span>
       </div>
       <Link href="/checkout">Place Order</Link>

b695f12ba65086cef624099f1cf1519738fe9193
AkshatG165 <guptaakshat3011@gmail.com>
2024-02-02 01:33:12 +0530

    feat: rating api

---
+++ pages/api/item/add-rating.ts
@@ -0,0 +1,60 @@
+import { connectToDB, getCollection } from '@/util/db';
+import { NextApiRequest, NextApiResponse } from 'next';
+import getSession from '../get-session';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === 'PATCH') {
+    const data: { itemName: string; rating: number }[] = req.body;
+    const session = await getSession(req, res);
+
+    if (!data) return res.status(422).json({ message: 'Invalid data' });
+
+    try {
+      const client = await connectToDB();
+      const itemsCollection = getCollection(client, 'items');
+
+      // Createing an array to store promises for each database operation
+      const promises = data.map(async (item) => {
+        const currItem = {
+          ...(await itemsCollection.findOne({ name: item.itemName })),
+        };
+        delete currItem._id;
+
+        const rating = {
+          userEmail: session?.user?.email,
+          rating: item.rating,
+        };
+
+        if (currItem.ratings) {
+          currItem.ratings.push(rating);
+          currItem.avgRating =
+            (currItem.avgRating * (currItem.ratings.length - 1) + item.rating) /
+            currItem.ratings.length;
+        } else {
+          currItem.ratings = [rating];
+          currItem.avgRating = item.rating;
+        }
+
+        // Replace the document in the collection & return that promise
+        return itemsCollection.replaceOne({ name: currItem.name }, currItem);
+      });
+
+      // Wait for all database operations to complete
+      await Promise.all(promises);
+      client.close();
+
+      return res.status(201).json({ message: 'Ratings updated' });
+    } catch (err) {
+      return res
+        .status(500)
+        .json({ message: 'Some error occoured, unable to update ratings' });
+    }
+  } else {
+    return res
+      .status(405)
+      .json({ message: `${req.method} method not supported` });
+  }
+}

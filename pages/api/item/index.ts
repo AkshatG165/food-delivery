a1903342b7fda24173fbe1e60af5a4efbc56be23
AkshatG165 <guptaakshat3011@gmail.com>
2024-02-02 20:57:44 +0530

    feat: New api for fetching items (for getting existing ratings)

--- pages/api/item/index.ts
+++ pages/api/item/index.ts
@@ -20,6 +20,24 @@ export default async function handler(
         .status(500)
         .json({ message: 'Some error occoured, unable to insert item' });
     }
+  } else if (req.method === 'GET') {
+    const { orderId } = req.query;
+
+    try {
+      const client = await connectToDB();
+      const result = await getCollection(client, 'items')
+        .find({ 'ratings.orderId': orderId })
+        .toArray();
+      client.close();
+
+      return res
+        .status(201)
+        .json({ message: 'Items fetched successfully', result: result });
+    } catch (e) {
+      return res
+        .status(500)
+        .json({ message: 'Some error occoured, unable to insert item' });
+    }
   } else {
     return res
       .status(405)

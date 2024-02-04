2967746d8729690d92f5a781111d319f78f941ac
AkshatG165 <guptaakshat3011@gmail.com>
2024-02-05 01:41:52 +0530

    feat: Back button in order details page

--- components/orders/OrderDetails.tsx
+++ components/orders/OrderDetails.tsx
@@ -2,6 +2,7 @@ import classes from './OrderDetails.module.css';
 import Card from '../ui/Card';
 import Order from '@/model/Order';
 import tick from '../../public/green-tick.jpg';
+import Link from 'next/link';
 
 function getDate(timestamp: number) {
   const date = new Date(timestamp * 1000);
@@ -31,52 +32,55 @@ export default function OrderDetails({ order }: { order: Order }) {
   ));
 
   return (
-    <Card className={classes['order-details']}>
-      <h3>Order Details</h3>
-      <div className={classes.item + ' ' + classes['date-img']}>
-        <span>{getDate(+order.orderDateTime)}</span>
-        <span>
-          {order.isDelivered ? 'Delivered' : 'On the way...'}
-          {order.isDelivered && <img src={tick.src} />}
-        </span>
-      </div>
-      <div className={classes.address}>
-        <div>
-          <b>Name - </b>
-          <span>{order.shippingAddress!.name}</span>
+    <div className={classes['order-details']}>
+      <Card className={classes.card}>
+        <h3>Order Details</h3>
+        <div className={classes.item + ' ' + classes['date-img']}>
+          <span>{getDate(+order.orderDateTime)}</span>
+          <span>
+            {order.isDelivered ? 'Delivered' : 'On the way...'}
+            {order.isDelivered && <img src={tick.src} />}
+          </span>
         </div>
-        <div>
-          <b>Address - </b>
-          <span>{order.shippingAddress!.address}, </span>
-          <span>{order.shippingAddress!.city}, </span>
-          <span>{order.shippingAddress!.state} - </span>
-          <span>{order.shippingAddress!.pincode}</span>
+        <div className={classes.address}>
+          <div>
+            <b>Name - </b>
+            <span>{order.shippingAddress!.name}</span>
+          </div>
+          <div>
+            <b>Address - </b>
+            <span>{order.shippingAddress!.address}, </span>
+            <span>{order.shippingAddress!.city}, </span>
+            <span>{order.shippingAddress!.state} - </span>
+            <span>{order.shippingAddress!.pincode}</span>
+          </div>
+          <div>
+            <b>Mobile No. - </b>
+            <span>{order.shippingAddress!.mobile} </span>
+          </div>
         </div>
-        <div>
-          <b>Mobile No. - </b>
-          <span>{order.shippingAddress!.mobile} </span>
+        <div className={classes['bill-details']}>
+          <h3>Bill</h3>
+          <ul>{itemList}</ul>
+          <b className={classes.item}>
+            <span>Subtotal</span>
+            <span>${cartTotal.toFixed(2)}</span>
+          </b>
+          <div className={classes.item}>
+            <span>GST</span>
+            <span>${gst.toFixed(2)}</span>
+          </div>
+          <div className={classes.item}>
+            <span>Delivery Charge</span>
+            <span>${deliveryCharge.toFixed(2)}</span>
+          </div>
+          <b className={classes.item + ' ' + classes.total}>
+            <span>Total Paid via {order.paymentMethod}</span>
+            <span>${order.totalPrice.toFixed(2)}</span>
+          </b>
         </div>
-      </div>
-      <div className={classes['bill-details']}>
-        <h3>Bill</h3>
-        <ul>{itemList}</ul>
-        <b className={classes.item}>
-          <span>Subtotal</span>
-          <span>${cartTotal.toFixed(2)}</span>
-        </b>
-        <div className={classes.item}>
-          <span>GST</span>
-          <span>${gst.toFixed(2)}</span>
-        </div>
-        <div className={classes.item}>
-          <span>Delivery Charge</span>
-          <span>${deliveryCharge.toFixed(2)}</span>
-        </div>
-        <b className={classes.item + ' ' + classes.total}>
-          <span>Total Paid via {order.paymentMethod}</span>
-          <span>${order.totalPrice.toFixed(2)}</span>
-        </b>
-      </div>
-    </Card>
+      </Card>
+      <Link href="/orders">Back</Link>
+    </div>
   );
 }

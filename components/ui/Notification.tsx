71c4f64e1207bb64735f8c0ee5700b3244a55cb6
AkshatG165 <guptaakshat3011@gmail.com>
2024-02-20 17:37:58 +0530

    style: Fixed color & close button

--- components/ui/Notification.tsx
+++ components/ui/Notification.tsx
@@ -1,25 +1,28 @@
 import classes from './Notification.module.css';
 import Card from './Card';
 import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../../store/index';
+import { hideNotification } from '@/store/notification-slice';
 
-type Props = {
-  type?: string;
-  message?: string;
-  setShowNotification: React.Dispatch<React.SetStateAction<boolean>>;
-};
+export default function Notification() {
+  const notification = useSelector(
+    (state: RootState) => state.notification.notification
+  );
+  const dispatch = useDispatch();
 
-export default function Notification({
-  type,
-  message,
-  setShowNotification,
-}: Props) {
-  const timer = 300000;
+  const timer = 5000;
   const [progress, setProgress] = useState(timer);
   const [isPaused, setIsPaused] = useState(false);
 
+  //reset timer if new notification comes
+  useEffect(() => {
+    setProgress(timer);
+  }, [notification.message]);
+
   useEffect(() => {
     if (progress <= 0) {
-      setShowNotification(false);
+      dispatch(hideNotification());
       return;
     }
     if (isPaused) return;
@@ -27,25 +30,27 @@ export default function Notification({
     return () => clearInterval(interval);
   }, [progress, isPaused]);
 
-  const handleClose = () => setShowNotification(false);
+  const handleClose = () => dispatch(hideNotification());
   const handleMouseEnter = () => setIsPaused(true);
   const handleMouseLeave = () => setIsPaused(false);
 
   return (
-    <div className={classes.backdrop}>
-      <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-        <Card
-          className={`${classes.notification} ${
-            type === 'success' ? classes.success : classes.failure
-          }`}
-        >
-          <button type="button" onClick={handleClose}>
-            x
-          </button>
-          <p>{message}</p>
-          <progress value={timer - progress} max={timer} />
-        </Card>
-      </div>
+    <div
+      className={classes.container}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <Card
+        className={`${classes.notification} ${
+          notification.type === 'success' ? classes.success : classes.failure
+        }`}
+      >
+        <button type="button" onClick={handleClose}>
+          x
+        </button>
+        <p>{notification.message}</p>
+        <progress value={timer - progress} max={timer} />
+      </Card>
     </div>
   );
 }

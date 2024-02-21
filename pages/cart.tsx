8812f2c04075dc41bc109024a609b7e8fd602d16
AkshatG165 <guptaakshat3011@gmail.com>
2024-02-21 12:09:41 +0530

    fix: Removed unnessasary code

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -2,11 +2,9 @@ import Cart from '@/components/cart/Cart';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getServerSession } from 'next-auth';
 import { authOptions } from './api/auth/[...nextauth]';
-import { connectToDB, getCollection } from '@/util/db';
-import { CartItem } from '@/model/CartItem';
 
-export default function ViewCart({ cartItems }: { cartItems: CartItem[] }) {
-  return <Cart cartItems={cartItems} />;
+export default function ViewCart() {
+  return <Cart />;
 }
 
 type Context = {
@@ -26,14 +24,7 @@ export async function getServerSideProps({ req, res }: Context) {
     };
   }
 
-  const client = await connectToDB();
-  const result = await getCollection(client, 'users').findOne({
-    email: session.user?.email,
-  });
-  const cartItems = result?.cartItems;
-  client.close();
-
   return {
-    props: { cartItems: cartItems },
+    props: {},
   };
 }
